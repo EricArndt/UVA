66cd6649f4027d543949de7776a471a4e6d7fe13
Ermiar007 <arndt.eric@gmail.com>
2012-11-05 14:48:02 -0500

    Should work now.

--- Root/Problem Set Volumes/Volume I/100 - The 3n + 1 problem/Main.java
+++ Root/Problem Set Volumes/Volume I/100 - The 3n + 1 problem/Main.java
@@ -4,34 +4,39 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-                long[] nums = new long[1000001];
-                
-                for (int i = 1; i <= 1000000; ++i) {
-                    long c = 0;
-                    long n = i;
-                    
-                    while (n != 1) {
-                        if (n % 2 == 0) {
-                            n /= 2;
-                        } else {
-                            n = 3 * n + 1;
-                        }
-                        ++c;
-                    }
-                    
-                    nums[i] = c;
-                }
-                
+		HashMap<Long, Long> nums = new HashMap<Long, Long>(); 
+		
+		for (long i = 1; i <= 1000000; ++i) {
+			long c = 0;
+			long n = i;
+					
+			while (n != 1) {
+				if (nums.containsKey(n)) {
+					c += nums.get(n);
+					break;
+				}else if (n % 2 == 0) {
+					n /= 2;
+				} else {
+					n = 3 * n + 1;
+				}
+
+				++c;
+			}
+					
+			nums.put(i, c);
+		}
+
 		while (s.hasNext()) {
 			int n = s.nextInt();
-                        int m = s.nextInt();
-                        long max = 0;
-                        
-                        for (int i = n; i <= m; ++i) {
-                            Math.max(max, nums[i]);
-                        }
+			int m = s.nextInt();
+			long max = 0;
+						
+			for (long i = n; i <= m; ++i) {
+				long getI = nums.get(i);
+				max = max > getI ? max : getI;
+			}
 			
-			System.out.println(n + " " + m + " " + max);
+			System.out.println(n + " " + m + " " + (max + 1L));
 		}
 	}
 }
