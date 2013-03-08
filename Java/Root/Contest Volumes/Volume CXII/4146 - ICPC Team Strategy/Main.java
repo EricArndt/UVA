0398628f4d63e5acd524929459af45e4b1b77fe5
Eric Arndt <arndt.eric@gmail.com>
2013-03-07 20:34:14 -0500

    Java ICPC Team Strategy Solution

---
+++ Java/Root/Contest Volumes/Volume CXII/4146 - ICPC Team Strategy/Main.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+public class Main {
+    static int currentSets;
+    static final int TIME = 280;
+    static final int PEOPLE = 3;
+    static int[] setMap;
+    static Problem[][] probs;
+    static int[][][] grid;
+    static int n;
+    
+    public static void main (String[] args) {     
+        Scanner s = new Scanner(System.in);
+           
+        int c = s.nextInt();
+        
+        final int totalSets = (int)Math.pow(2, 12);
+        grid = new int[totalSets][TIME + 1][3];
+        
+        for (int cases = 0; cases < c; ++cases) {
+            n = s.nextInt();
+            
+            currentSets = (int)Math.pow(2, n);
+            int fastestPerson = 0;
+            int fastestTime = Integer.MAX_VALUE;
+            
+            probs = new Problem[PEOPLE][n];
+            
+            for (int i = 0; i < 3; ++i) {
+                probs[i] = new Problem[n];
+                
+                for (int j = 0; j < n; ++j) {
+                    probs[i][j] = new Problem(j, s.nextInt());
+                    if (probs[i][j].time < fastestTime) {
+                        fastestPerson = i;
+                        fastestTime = probs[i][j].time;
+                    }
+                }
+                
+                Arrays.sort(probs[i]);
+            }
+            
+            // reset
+            for (int i = 0; i < currentSets; ++i) {
+                for (int j = 0; j <= TIME; ++j) {
+                    for (int k = 0; k < PEOPLE; ++k) {
+                        grid[i][j][k] = Integer.MIN_VALUE;
+                    }
+                }
+            }
+            
+            // do DP with memoization
+            int best = Math.max(recursiveDP(currentSets - 1, TIME, 0), Math.max(recursiveDP(currentSets - 1, TIME, 1), recursiveDP(currentSets - 1, TIME, 2)));
+            System.out.println(best);
+        }
+    }
+    
+    static int recursiveDP(int set, int time ,int solver) {
+        // we're outside the bounds
+        if (set < 0 || time < 0) {
+            return 0;
+        }
+        
+        // we already calculated it
+        if (grid[set][time][solver] != Integer.MIN_VALUE) {
+            return grid[set][time][solver];
+        }
+        
+        // calculate the best we can do
+        int best = 0;
+        int bestIfSove = 0;
+        // can you solve a problem with the remaining set?
+        for (int i = 0; i < n; ++i) {
+            // best problem for you to solve
+            if ((set & (1 << probs[solver][i].id)) > 0) {
+                if (probs[solver][i].time <= time) {
+                    // you can solve your best problem left
+                    int solveTime = probs[solver][i].time;
+                    int setWithoutProblem = set & ~(1 << probs[solver][i].id);
+                    bestIfSove = Math.max(0, Math.max(recursiveDP(setWithoutProblem, time - solveTime, (solver + 1) % PEOPLE), recursiveDP(setWithoutProblem, time - solveTime, (solver + 2) % PEOPLE))) + 1;
+                    break;
+                }
+            }
+        }
+        
+        best = bestIfSove;
+        // save in memo table
+        grid[set][time][solver] = best;
+        
+        return best;
+    }
+    
+    static class Problem implements Comparable<Problem> {
+        int id;
+        int time;
+        
+        Problem(int id, int time) {
+            this.id = id;
+            this.time = time;
+        }
+
+        public int compareTo(Problem t) {
+            return ((Integer)this.time).compareTo(t.time);
+        }
+    }
+}
